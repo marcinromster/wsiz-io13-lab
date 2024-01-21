b8b9b0045ebbabb45fb9c75538ff2d494880b543
mariusztymowicz <mariusz.tymowicz@gmail.com>
2024-01-21 10:31:34 +0100

    2.2 done

--- src/pl/edu/wsiz/io13/part2/Employee.java
+++ src/pl/edu/wsiz/io13/part2/Employee.java
@@ -4,4 +4,8 @@ public class Employee {
     String firstname;
     String lastname;
     byte age;
+
+    void print() {
+        System.out.printf("Employee: %s %s %d", firstname, lastname, age);
+    }
 }
--- src/pl/edu/wsiz/io13/part2/Program.java
+++ src/pl/edu/wsiz/io13/part2/Program.java
@@ -2,13 +2,17 @@ package pl.edu.wsiz.io13.part2;
 
 public class Program {
     public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        employee1.firstname = "Jan";
+        employee1.lastname = "Kowalski";
+        employee1.age = 30;
 
-        Employee employee = new Employee();
+        Employee employee2 = new Employee();
+        employee2.firstname = "Anna";
+        employee2.lastname = "Nowak";
+        employee2.age = 28;
 
-        employee.firstname = "Jan";
-        employee.lastname = "Kowalski";
-        employee.age = 30;
-
-        System.out.printf("Employee: %s %s %d", employee.firstname, employee.lastname, employee.age);
+        employee1.print();
+        employee2.print();
     }
 }
