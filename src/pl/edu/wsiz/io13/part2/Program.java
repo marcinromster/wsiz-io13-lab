5214c391cf9a73dd4284247b23d9ece48154a7ed
mariusztymowicz <mariusz.tymowicz@gmail.com>
2024-02-04 09:04:55 +0100

    2.10 v2

--- src/pl/edu/wsiz/io13/part2/Program.java
+++ src/pl/edu/wsiz/io13/part2/Program.java
@@ -1,7 +1,10 @@
 package pl.edu.wsiz.io13.part2;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Company company = new Company();
 
         company.add(new Employee("Jan", "Kowalski", (byte) 30));
@@ -15,6 +18,23 @@ public class Program {
         System.out.println("3 - usuń pracownika");
         System.out.println("9 - zakończ program");
 
-        System.out.println("Podaj numer operacji:");
+        System.out.print("Podaj numer operacji: ");
+        int operationNumber = scanner.nextInt();
+
+        if (operationNumber == 1) {
+            company.printEmployees();
+        }
+        if (operationNumber == 2) {
+            company.add();
+        }
+        if (operationNumber == 3) {
+            System.out.print("Podaj numer pracownika do usunięcia: ");
+            int employeeNumber = scanner.nextInt();
+
+            company.removeEmployee(employeeNumber);
+        }
+        if (operationNumber == 9) {
+            // exit
+        }
     }
 }
